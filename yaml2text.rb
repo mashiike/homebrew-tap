1902694143816c228f4f7eed52134b7de130e18b
mashiike <ikeda-masashi@kayac.com>
2020-06-10 18:02:10 +0900

    update yaml2text v0.1.1

--- yaml2text.rb
+++ yaml2text.rb
@@ -1,8 +1,8 @@
 class Yaml2text < Formula
-  version '0.1.0'
+  version '0.1.1'
   homepage 'https://github.com/mashiike/yaml2text'
-  url "https://github.com/mashiike/yaml2text/releases/download/v0.1.0/yaml2text-v0.1.0-darwin-amd64.zip"
-  sha256 '24116a18621bfeb395b2c3193f5c6ec3d4da65d9c73da250dbc322806df1b1c0'
+  url "https://github.com/mashiike/yaml2text/releases/download/v0.1.1/yaml2text_v0.1.1_darwin_amd64.zip"
+  sha256 'eb4e4fc6bb03f27fabab3ff70b5537298bff7ff3820c88cf86603e33be4c8c0e'
   head 'https://github.com/mashiike/yaml2text.git'
 
   head do
