71f61ad37407287aadf54e10f7e83cf4dc42d4a2
mashiike <ikeda-masashi@kayac.com>
2021-11-01 21:07:12 +0900

    shimesaba v0.2.1

--- shimesaba.rb
+++ shimesaba.rb
@@ -1,8 +1,8 @@
 class Shimesaba < Formula
-  version '0.2.0'
+  version '0.2.1'
   homepage 'https://github.com/mashiike/shimesaba'
-  url "https://github.com/mashiike/shimesaba/releases/download/v0.2.0/shimesaba_0.2.0_darwin_amd64.tar.gz"
-  sha256 '180383ebaefd65e60b8a48366bb5049afc65040c1020a47067c316357a6248a3'
+  url "https://github.com/mashiike/shimesaba/releases/download/v0.2.1/shimesaba_0.2.1_darwin_amd64.tar.gz"
+  sha256 '19ee5beffefed3db572ea74a674dda014d8f7069dbd3e16997fdf76b0c1f3a40'
   head 'https://github.com/mashiike/shimesaba.git'
 
   head do
