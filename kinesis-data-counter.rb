aac21b9c424cf7cd109d02d5e6b0840a644e02b0
mashiike <ikeda-masashi@kayac.com>
2021-12-01 16:51:31 +0900

    kinesis-data-counter v0.2.0

--- kinesis-data-counter.rb
+++ kinesis-data-counter.rb
@@ -1,13 +1,13 @@
 class KinesisDataCounter < Formula
-  version '0.1.4'
+  version '0.2.0'
   homepage 'https://github.com/mashiike/kinesis-data-counter'
   if OS.mac?
-    url "https://github.com/mashiike/kinesis-data-counter/releases/download/v0.1.4/kinesis-data-counter_0.1.4_darwin_amd64.tar.gz"
-    sha256 '73fa9bc3836e08bd61f1ac34067a779f7693ff3bb534b569a29105203926f194'
+    url "https://github.com/mashiike/kinesis-data-counter/releases/download/v0.2.0/kinesis-data-counter_0.2.0_darwin_amd64.tar.gz"
+    sha256 '593853a9ee5fc722c2aedbe0ba420e007a98f8153bc0cc1519d480d33df90a4f'
   end
   if OS.linux?
-    url "https://github.com/mashiike/kinesis-data-counter/releases/download/v0.1.4/kinesis-data-counter_0.1.4_linux_amd64.tar.gz"
-    sha256 '9e9483619f22a0721532b2e80a238a970d57cbd34233b58283f7ae32615abcf6'
+    url "https://github.com/mashiike/kinesis-data-counter/releases/download/v0.2.0/kinesis-data-counter_0.2.0_linux_amd64.tar.gz"
+    sha256 'd08ec5c4d2c2c66b39bd9a12e9b1681ee25e7a8818f45b960c4617e592cfbbc0'
   end
   head 'https://github.com/mashiike/kinesis-data-counter.git'
 
