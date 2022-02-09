1a0f3eb7ef000afbe40873771405e40c7cc63c53
mashiike <ikeda-masashi@kayac.com>
2022-02-09 14:09:52 +0900

    update-all

--- shimesaba.rb
+++ shimesaba.rb
@@ -1,8 +1,8 @@
 class Shimesaba < Formula
-  version '0.7.0'
+  version '0.7.2'
   homepage 'https://github.com/mashiike/shimesaba'
-  url "https://github.com/mashiike/shimesaba/releases/download/v0.7.0/shimesaba_0.7.0_darwin_amd64.tar.gz"
-  sha256 '43c6a7e4bf8194a6e4de2e491c814514835d052c4f6e9f0a50bb3821e81e2fad'
+  url "https://github.com/mashiike/shimesaba/releases/download/v0.7.2/shimesaba_0.7.2_darwin_amd64.tar.gz"
+  sha256 '316f0ce8dc1fd1f1faf535b1f7ee5158f55588b55f4572bb694a157b101f0e64'
   head 'https://github.com/mashiike/shimesaba.git'
 
   head do
--- stefunny.rb
+++ stefunny.rb
@@ -1,13 +1,13 @@
 class Stefunny < Formula
-  version '0.1.1'
+  version '0.2.0'
   homepage 'https://github.com/mashiike/stefunny'
   if OS.mac?
-    url "https://github.com/mashiike/stefunny/releases/download/v0.1.1/stefunny_0.1.1_darwin_amd64.tar.gz"
-    sha256 '8fe6543e4f5e6f23ccdcac0c914d9dbf353970914f300a35b1171c221223d590'
+    url "https://github.com/mashiike/stefunny/releases/download/v0.2.0/stefunny_0.2.0_darwin_amd64.tar.gz"
+    sha256 '98ce0720691ced8489093ba658b1927aef92861edfc74f613492386a5be8df61'
   end
   if OS.linux?
-    url "https://github.com/mashiike/stefunny/releases/download/v0.1.1/stefunny_0.1.1_linux_amd64.tar.gz"
-    sha256 'd884beab062ba7324ed8a3083f95569bd537d169017d14c81d0ccbd7a88da188'
+    url "https://github.com/mashiike/stefunny/releases/download/v0.2.0/stefunny_0.2.0_linux_amd64.tar.gz"
+    sha256 'a8fb751dbd7d9781891466f1254a64c4ac0bdeba93059debcda28fd8cfc16b90'
   end
   head 'https://github.com/mashiike/stefunny.git'
 
