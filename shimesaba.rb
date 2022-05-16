1a55992571136063a54f6cdc6e53f9ef6ad718bf
mashiike <ikeda-masashi@kayac.com>
2022-05-16 10:14:21 +0900

    add shimesaba linux

--- shimesaba.rb
+++ shimesaba.rb
@@ -1,8 +1,14 @@
 class Shimesaba < Formula
   version '1.0.0'
   homepage 'https://github.com/mashiike/shimesaba'
-  url "https://github.com/mashiike/shimesaba/releases/download/v1.0.0/shimesaba_1.0.0_darwin_amd64.tar.gz"
-  sha256 'd429976458d36cfc5e6d37d8dff1945e90352fb6d98c06366cb752af91b77a4b'
+  if OS.mac?
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.0.0/shimesaba_1.0.0_darwin_amd64.tar.gz"
+    sha256 'd429976458d36cfc5e6d37d8dff1945e90352fb6d98c06366cb752af91b77a4b'
+  end
+  if OS.linux?
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.0.0/shimesaba_1.0.0_linux_amd64.tar.gz"
+    sha256 '2b2c9eaa3c9183b57bed94158b9aa535e5bb9713589cf67668de4d9024d7ee86'
+  end
   head 'https://github.com/mashiike/shimesaba.git'
 
   head do
