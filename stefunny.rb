e197a732926f2367659a1d48896fc08bd84417fe
mashiike <ikeda-masashi@kayac.com>
2022-05-18 17:23:17 +0900

    stefunny v0.4.3

--- stefunny.rb
+++ stefunny.rb
@@ -1,13 +1,13 @@
 class Stefunny < Formula
-  version '0.4.2'
+  version '0.4.3'
   homepage 'https://github.com/mashiike/stefunny'
   if OS.mac?
-    url "https://github.com/mashiike/stefunny/releases/download/v0.4.2/stefunny_0.4.2_darwin_amd64.tar.gz"
-    sha256 'eaeb76a5890be769b3269e9e77d2d4544f825be4a5abd8fc90a03bdb6a7fb412'
+    url "https://github.com/mashiike/stefunny/releases/download/v0.4.3/stefunny_0.4.3_darwin_amd64.tar.gz"
+    sha256 'd366d781d11d00094e2fc5d1925f9c2ac748b9bb580c2e5c9cd449bb98c7a9af'
   end
   if OS.linux?
-    url "https://github.com/mashiike/stefunny/releases/download/v0.4.2/stefunny_0.4.2_linux_amd64.tar.gz"
-    sha256 '80fd2d3088b0dcae70e3bafcf5ee8f2734af4a9ad5b0ff3dd4841e86ba7c3d1f'
+    url "https://github.com/mashiike/stefunny/releases/download/v0.4.3/stefunny_0.4.3_linux_amd64.tar.gz"
+    sha256 '0c4bf299e65164bbc76abca51604d33b64f1a9b8a0142637465ac1696d78715a'
   end
   head 'https://github.com/mashiike/stefunny.git'
 
