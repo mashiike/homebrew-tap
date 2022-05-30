8b39442c4276c73ca9b23222c0dbf446f846e23f
mashiike <ikeda-masashi@kayac.com>
2022-05-30 20:39:58 +0900

    update shimesaba v1.1.1

--- shimesaba.rb
+++ shimesaba.rb
@@ -1,13 +1,13 @@
 class Shimesaba < Formula
-  version '1.1.0'
+  version '1.1.1'
   homepage 'https://github.com/mashiike/shimesaba'
   if OS.mac?
-    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.0/shimesaba_1.1.0_darwin_amd64.tar.gz"
-    sha256 '94debc4eac1d6870bc4af697017607d361bbc8dbbdb2e1a792618cf554e3ca36'
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.1/shimesaba_1.1.1_darwin_amd64.tar.gz"
+    sha256 '9549f841bf52091d2c9771af5c246255815ab712d5f6ed87588671ca43466725'
   end
   if OS.linux?
-    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.0/shimesaba_1.1.0_linux_amd64.tar.gz"
-    sha256 'd4c590a2f0afd028d82b939f06efbfb219adc5d99ddd31ff71c986ffbdb07149'
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.1/shimesaba_1.1.1_linux_amd64.tar.gz"
+    sha256 'f4072e1e6ea7ed3313f3acc082081afcaef68214a250b4c7ddcaefcedcd6cb01'
   end
   head 'https://github.com/mashiike/shimesaba.git'
 
