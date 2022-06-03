ac63ebb742e2bb78ae31d2413ac87afe9eca3dcc
mashiike <ikeda-masashi@kayac.com>
2022-06-03 19:17:23 +0900

    awstee v0.2.0

--- awstee.rb
+++ awstee.rb
@@ -1,13 +1,13 @@
 class Awstee < Formula
-  version '0.1.0'
+  version '0.2.0'
   homepage 'https://github.com/mashiike/awstee'
   if OS.mac?
-    url "https://github.com/mashiike/awstee/releases/download/v0.1.0/awstee_0.1.0_darwin_amd64.tar.gz"
-    sha256 '109de23257ad8dd1e623581f67c2081f129f5cc4f1aeed8ddd99130725295099'
+    url "https://github.com/mashiike/awstee/releases/download/v0.2.0/awstee_0.2.0_darwin_amd64.tar.gz"
+    sha256 'c70b69fd93c8edb4b3c44c4e923824bfb43f157acbffe6411708123e978d72fd'
   end
   if OS.linux?
-    url "https://github.com/mashiike/awstee/releases/download/v0.1.0/awstee_0.1.0_linux_amd64.tar.gz"
-    sha256 '9105d2fbee3b1781d114465c12f8a38211971d6ec14833b162029b28baa7a777'
+    url "https://github.com/mashiike/awstee/releases/download/v0.2.0/awstee_0.2.0_linux_amd64.tar.gz"
+    sha256 '17fc6eab695696561d6936b4364860c8f36c3e19e56d59f59b464929edd1e48f'
   end
   head 'https://github.com/mashiike/awstee.git'
 
