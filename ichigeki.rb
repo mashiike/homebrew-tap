43af565eab5c1015a02c81f2cc30af3e23f22984
mashiike <ikeda-masashi@kayac.com>
2022-06-04 13:06:19 +0900

    update ichigeki v0.3.0

--- ichigeki.rb
+++ ichigeki.rb
@@ -1,13 +1,13 @@
 class Ichigeki < Formula
-  version '0.2.0'
+  version '0.3.0'
   homepage 'https://github.com/mashiike/ichigeki'
   if OS.mac?
-    url "https://github.com/mashiike/ichigeki/releases/download/v0.2.0/ichigeki_0.2.0_darwin_amd64.tar.gz"
-    sha256 '87bba74fb85cd9461b8609aa26da2a225500797eb339267372fb532c20ae5275'
+    url "https://github.com/mashiike/ichigeki/releases/download/v0.3.0/ichigeki_0.3.0_darwin_amd64.tar.gz"
+    sha256 '6e016a8dd94f55e9e0039d78057b82012a8c09769b2e1dff354edb2df0a8dba9'
   end
   if OS.linux?
-    url "https://github.com/mashiike/ichigeki/releases/download/v0.2.0/ichigeki_0.2.0_linux_amd64.tar.gz"
-    sha256 '5879e191e53ac9b0a5124ebd6af4e90d1f219240dbf2eab3c71402f644e8355a'
+    url "https://github.com/mashiike/ichigeki/releases/download/v0.3.0/ichigeki_0.3.0_linux_amd64.tar.gz"
+    sha256 'c215ad4f9459dbe76db422254b20300bfd9a8467da0d038e7380dbb1265d1c78'
   end
   head 'https://github.com/mashiike/ichigeki.git'
 
