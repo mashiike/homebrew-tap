989e409e6753285e94b1ea67f303f4dfd817e3bb
mashiike <ikeda-masashi@kayac.com>
2022-07-20 10:03:10 +0900

    update-all

--- gdnotify.rb
+++ gdnotify.rb
@@ -1,13 +1,13 @@
 class Gdnotify < Formula
-  version '0.1.2'
+  version '0.2.0'
   homepage 'https://github.com/mashiike/gdnotify'
   if OS.mac?
-    url "https://github.com/mashiike/gdnotify/releases/download/v0.1.2/gdnotify_0.1.2_darwin_amd64.tar.gz"
-    sha256 '43047f18fe8a5ea4ffc79765f43962b602ee672106f9c252560f263850eddf86'
+    url "https://github.com/mashiike/gdnotify/releases/download/v0.2.0/gdnotify_0.2.0_darwin_amd64.tar.gz"
+    sha256 '40ce96da3e08666b70d29ba8567f6897e364798ca6027f1e5b20cc5961a78ff5'
   end
   if OS.linux?
-    url "https://github.com/mashiike/gdnotify/releases/download/v0.1.2/gdnotify_0.1.2_linux_amd64.tar.gz"
-    sha256 '7e407c98f3884c39d44f9cb68e4aa07ed36bb367670fc548d6a351a298b0dc34'
+    url "https://github.com/mashiike/gdnotify/releases/download/v0.2.0/gdnotify_0.2.0_linux_amd64.tar.gz"
+    sha256 'a34d0416cd7e85919f1fd4035482108544ae0f92f5b6fad33a148eebbe2d2783'
   end
   head 'https://github.com/mashiike/gdnotify.git'
 
--- shimesaba.rb
+++ shimesaba.rb
@@ -1,13 +1,13 @@
 class Shimesaba < Formula
-  version '1.1.1'
+  version '1.2.0'
   homepage 'https://github.com/mashiike/shimesaba'
   if OS.mac?
-    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.1/shimesaba_1.1.1_darwin_amd64.tar.gz"
-    sha256 '9549f841bf52091d2c9771af5c246255815ab712d5f6ed87588671ca43466725'
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.2.0/shimesaba_1.2.0_darwin_amd64.tar.gz"
+    sha256 'be6942bb3fd153a1a017700c69fc20102ff929727a4ddd9091ad13b7cfcd5be2'
   end
   if OS.linux?
-    url "https://github.com/mashiike/shimesaba/releases/download/v1.1.1/shimesaba_1.1.1_linux_amd64.tar.gz"
-    sha256 'f4072e1e6ea7ed3313f3acc082081afcaef68214a250b4c7ddcaefcedcd6cb01'
+    url "https://github.com/mashiike/shimesaba/releases/download/v1.2.0/shimesaba_1.2.0_linux_amd64.tar.gz"
+    sha256 '89d76d784c0146c1054aececeacf41349ba5b07bea656671baff5abacfe73faa'
   end
   head 'https://github.com/mashiike/shimesaba.git'
 
