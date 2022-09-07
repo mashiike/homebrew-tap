8ac89d972ad8b79813236ce9b9ef6746f93a2059
mashiike <ikeda-masashi@kayac.com>
2022-09-08 03:18:51 +0900

    update

--- prepalert.rb
+++ prepalert.rb
@@ -1,13 +1,13 @@
 class Prepalert < Formula
-  version '0.3.0'
+  version '0.3.1'
   homepage 'https://github.com/mashiike/prepalert'
   if OS.mac?
-    url "https://github.com/mashiike/prepalert/releases/download/v0.3.0/prepalert_0.3.0_darwin_amd64.tar.gz"
-    sha256 'c578f8289dd4d08feb1932f4aaf509d9b6b705b4318c8528ad9410475df41090'
+    url "https://github.com/mashiike/prepalert/releases/download/v0.3.1/prepalert_0.3.1_darwin_amd64.tar.gz"
+    sha256 '84df212812604d172882a91fe68b48c78f7b3ec4144007e0ea398fc01a616d35'
   end
   if OS.linux?
-    url "https://github.com/mashiike/prepalert/releases/download/v0.3.0/prepalert_0.3.0_linux_amd64.tar.gz"
-    sha256 '30d44f3787e3486d80df2b03584afd0fb49004e29b1f6828b687fca1e2fb7bc9'
+    url "https://github.com/mashiike/prepalert/releases/download/v0.3.1/prepalert_0.3.1_linux_amd64.tar.gz"
+    sha256 '587d7ebcac638a6fa7641ad2433d19b2318080672e32b6f38093092db5b64eeb'
   end
   head 'https://github.com/mashiike/prepalert.git'
 
