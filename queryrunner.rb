cc83d935eba00ea91adca35116b1a938e7d1b1ea
mashiike <ikeda-masashi@kayac.com>
2022-10-14 15:58:30 +0900

    queryrunner

--- queryrunner.rb
+++ queryrunner.rb
@@ -1,13 +1,13 @@
 class Queryrunner < Formula
-  version '0.1.1'
+  version '0.2.1'
   homepage 'https://github.com/mashiike/queryrunner'
   if OS.mac?
-    url "https://github.com/mashiike/queryrunner/releases/download/v0.1.1/queryrunner_0.1.1_darwin_amd64.tar.gz"
-    sha256 '3864220bb904d9fcb5dc94157d1823cca1b0246be646985d66c6933929c90873'
+    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.1/queryrunner_0.2.1_darwin_amd64.tar.gz"
+    sha256 '3c6f71fa5b130b96e7ea896f5b2c6f16bde062b84af53e721741bac5fae3faa5'
   end
   if OS.linux?
-    url "https://github.com/mashiike/queryrunner/releases/download/v0.1.1/queryrunner_0.1.1_linux_amd64.tar.gz"
-    sha256 'f67af57235b9136e3dafbbbc4667f2509e3ce09c89fb8b92a38a98355a9fc86c'
+    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.1/queryrunner_0.2.1_linux_amd64.tar.gz"
+    sha256 'e84ea2ab100b6f730865a6a3da7d98f9e8d181f478107de862f9ac744f6187de'
   end
   head 'https://github.com/mashiike/queryrunner.git'
 
