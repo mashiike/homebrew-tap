7739490a050199d8a10bbdbf35ae267b53543db9
mashiike <ikeda-masashi@kayac.com>
2022-10-14 17:46:21 +0900

    update all

--- prepalert.rb
+++ prepalert.rb
@@ -1,13 +1,13 @@
 class Prepalert < Formula
-  version '0.6.1'
+  version '0.7.0'
   homepage 'https://github.com/mashiike/prepalert'
   if OS.mac?
-    url "https://github.com/mashiike/prepalert/releases/download/v0.6.1/prepalert_0.6.1_darwin_amd64.tar.gz"
-    sha256 '16d1fd370eaa455cf02f6745ce793518865b44a804be67cacd4a46cb0e8aa666'
+    url "https://github.com/mashiike/prepalert/releases/download/v0.7.0/prepalert_0.7.0_darwin_amd64.tar.gz"
+    sha256 '5149c95d224d7ae748a5b9586c8998f43c89f2ecaf34410c13ba07257ebb6a4e'
   end
   if OS.linux?
-    url "https://github.com/mashiike/prepalert/releases/download/v0.6.1/prepalert_0.6.1_linux_amd64.tar.gz"
-    sha256 'fa72766038f4290dda13148de005f78fd141d97cd0dbfc5a7ead55cb192a2407'
+    url "https://github.com/mashiike/prepalert/releases/download/v0.7.0/prepalert_0.7.0_linux_amd64.tar.gz"
+    sha256 'b783d45eb146ba512504fa2a8e35b4ab09717db80eba404cbc01020f77214322'
   end
   head 'https://github.com/mashiike/prepalert.git'
 
--- queryrunner.rb
+++ queryrunner.rb
@@ -1,13 +1,13 @@
 class Queryrunner < Formula
-  version '0.2.1'
+  version '0.2.2'
   homepage 'https://github.com/mashiike/queryrunner'
   if OS.mac?
-    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.1/queryrunner_0.2.1_darwin_amd64.tar.gz"
-    sha256 '3c6f71fa5b130b96e7ea896f5b2c6f16bde062b84af53e721741bac5fae3faa5'
+    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.2/queryrunner_0.2.2_darwin_amd64.tar.gz"
+    sha256 '8f562c8de935d91dcafd58d13f7bbf001c962420051950705945a068f84fffb0'
   end
   if OS.linux?
-    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.1/queryrunner_0.2.1_linux_amd64.tar.gz"
-    sha256 'e84ea2ab100b6f730865a6a3da7d98f9e8d181f478107de862f9ac744f6187de'
+    url "https://github.com/mashiike/queryrunner/releases/download/v0.2.2/queryrunner_0.2.2_linux_amd64.tar.gz"
+    sha256 '06d5ebca508320f1cc99b123ecec7e61b23e7867899dcb9c31bfd1d44b08f55e'
   end
   head 'https://github.com/mashiike/queryrunner.git'
 
