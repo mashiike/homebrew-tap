55edd534e6536a73c0c64de6cbe4cafcd62d1252
mashiike <ikeda-masashi@kayac.com>
2022-11-14 15:20:58 +0900

    update rc

--- redshift-credentials.rb
+++ redshift-credentials.rb
@@ -1,27 +1,27 @@
 class RedshiftCredentials < Formula
   desc 'a command line tool for Amazon Redshift temporary authorization with AWS IAM '
-  version '0.2.0'
+  version '0.3.0'
   homepage 'https://github.com/mashiike/redshift-credentials'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.2.0/redshift-credentials_0.2.0_darwin_arm64.tar.gz'
-      sha256 '84b743a51b206d7b41a8db41107e949f64b3e98bcba09086dd448a91e73cc943'
+      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.3.0/redshift-credentials_0.3.0_darwin_arm64.tar.gz'
+      sha256 '5497e3873f6a05157c2a3337452a0da56aa4668aec7c1422f2ac8dd4172e2eb6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.2.0/redshift-credentials_0.2.0_darwin_amd64.tar.gz'
-      sha256 '4fbb53dbb747b2225b6d7fee4a6cc936d25d6f6bd798b81f2d9b0b8565e64d4e'
+      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.3.0/redshift-credentials_0.3.0_darwin_amd64.tar.gz'
+      sha256 '24f2b07600a2f0154ecd18daf5066f6143b2c5dff2f3397217e89c88a9814f55'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.2.0/redshift-credentials_0.2.0_linux_arm64.tar.gz'
-      sha256 '3d1b9b4561786a0b564075589de2ee3e3c4eed6c7fb368145e2e9e9b5edf22a6'
+      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.3.0/redshift-credentials_0.3.0_linux_arm64.tar.gz'
+      sha256 '2b9aa360a48e884be4183ce8e3ee5a4b67794a521cab41c1e4566881fbf61be3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.2.0/redshift-credentials_0.2.0_linux_amd64.tar.gz'
-      sha256 'cc868444ec8042d08acb4f786205c089931d048c0128cf63480c9670dbb1302e'
+      url 'https://github.com/mashiike/redshift-credentials/releases/download/v0.3.0/redshift-credentials_0.3.0_linux_amd64.tar.gz'
+      sha256 'ec156e21c4152a1f3cd5718b5a9cbc3390141da3ea85dd08e272c4ea634d7ea0'
     end
   end
 
