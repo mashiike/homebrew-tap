0c1232975c6806565c8f9abedc2c2b9d84ac9db7
mashiike <ikeda-masashi@kayac.com>
2022-12-22 14:04:41 +0900

    update all

--- prepalert.rb
+++ prepalert.rb
@@ -1,27 +1,27 @@
 class Prepalert < Formula
   desc 'Toil reduction tool to prepare before responding to Mackerel alerts'
-  version '0.10.0'
+  version '0.10.1'
   homepage 'https://github.com/mashiike/prepalert'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.0/prepalert_0.10.0_darwin_arm64.tar.gz'
-      sha256 'ce0958b4b65bacca6cf7b10e9774155ee254e536939fde0a9554c29450257448'
+      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.1/prepalert_0.10.1_darwin_arm64.tar.gz'
+      sha256 'a8f48e5e16f09c9066a321fa0ff868d298294e53a50b7bd7afefd481098a9334'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.0/prepalert_0.10.0_darwin_amd64.tar.gz'
-      sha256 '23c6da3058810c0165489ac0288aeff866cb587b2f561af9d2e09449228ef969'
+      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.1/prepalert_0.10.1_darwin_amd64.tar.gz'
+      sha256 '30df5cb10385464f8cdb2d55c6cb8c330089238acb3c3535a9468ca36b81ea91'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.0/prepalert_0.10.0_linux_arm64.tar.gz'
-      sha256 '6867bb3f205aba0b5928402119100b060648b7a05982c36df0d706374a341380'
+      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.1/prepalert_0.10.1_linux_arm64.tar.gz'
+      sha256 '3d95804c56c37f3aef47b5236f84b11ca10ffe2249f02855e8099dba58505a36'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.0/prepalert_0.10.0_linux_amd64.tar.gz'
-      sha256 '14873cde37a2910f83c40558365c345b3a91b3e539ea0e4a47a15e20c5aaaf83'
+      url 'https://github.com/mashiike/prepalert/releases/download/v0.10.1/prepalert_0.10.1_linux_amd64.tar.gz'
+      sha256 '92b4f833f7f442053e0b29184e43de89f3af20b73e5e39d1426a4e11d8ec2031'
     end
   end
 
--- queryrunner.rb
+++ queryrunner.rb
@@ -1,26 +1,26 @@
 class Queryrunner < Formula
-  version '0.2.3'
+  version '0.3.0'
   homepage 'https://github.com/mashiike/queryrunner'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/queryrunner/releases/download/v0.2.3/queryrunner_0.2.3_darwin_arm64.tar.gz'
-      sha256 'c56724d5159ff16ec395bb384fe03db424266453be4b8653a8a1cad9cf0b40fd'
+      url 'https://github.com/mashiike/queryrunner/releases/download/v0.3.0/queryrunner_0.3.0_darwin_arm64.tar.gz'
+      sha256 'fd8c34473d256a2b34ce05515fd53feea17e091e965486e52fe01841916e5a84'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/queryrunner/releases/download/v0.2.3/queryrunner_0.2.3_darwin_amd64.tar.gz'
-      sha256 '41efa1a4597efe7323e8b24ba42ee235c221ce45839da07661e100e92e9396ce'
+      url 'https://github.com/mashiike/queryrunner/releases/download/v0.3.0/queryrunner_0.3.0_darwin_amd64.tar.gz'
+      sha256 '50da6efda4f8e0465c96924404ab4cb9e94161f913e48d70b0938024ea70bc11'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/queryrunner/releases/download/v0.2.3/queryrunner_0.2.3_linux_arm64.tar.gz'
-      sha256 '6e00db7a2d77ba0a62afbac19682f1f3a78247a4a9681e7f1563f285759a311b'
+      url 'https://github.com/mashiike/queryrunner/releases/download/v0.3.0/queryrunner_0.3.0_linux_arm64.tar.gz'
+      sha256 'c028d560047053f3620ab3fc915646174dd8e1fdddd53f57f78716deb6e829b0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/queryrunner/releases/download/v0.2.3/queryrunner_0.2.3_linux_amd64.tar.gz'
-      sha256 '83fee13bc49743dab1403ea91e66dae71507602170e119d4fb3c566f15fcb19d'
+      url 'https://github.com/mashiike/queryrunner/releases/download/v0.3.0/queryrunner_0.3.0_linux_amd64.tar.gz'
+      sha256 '9e1adf7aea805787bda8b02ff8e79cf162e7faa99bbf395b19b6a618c76e47c6'
     end
   end
 
