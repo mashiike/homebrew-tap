79f741c0eab55277b15728365aa5e1c114bee28f
mashiike <ikeda-masashi@kayac.com>
2023-03-06 10:36:41 +0900

    update gdnotify v0.4.0

--- gdnotify.rb
+++ gdnotify.rb
@@ -1,27 +1,27 @@
 class Gdnotify < Formula
   desc 'google drive change notifier for AWS'
-  version '0.3.0'
+  version '0.4.0'
   homepage 'https://github.com/mashiike/gdnotify'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/gdnotify/releases/download/v0.3.0/gdnotify_0.3.0_darwin_arm64.tar.gz'
-      sha256 'e0793c7a3088e8b496424058c966738ff4c041625cf37351d204a540f989c31a'
+      url 'https://github.com/mashiike/gdnotify/releases/download/v0.4.0/gdnotify_0.4.0_darwin_arm64.tar.gz'
+      sha256 'fd5ff3babbd23db48b0506b25013f8fc673ffe5bc29ddee0a119d37f8236999a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/gdnotify/releases/download/v0.3.0/gdnotify_0.3.0_darwin_amd64.tar.gz'
-      sha256 'c7dd63b962b6113ffa7a3dade591ff9fb2f69f7e31fd3e55cfab9b4397cc91d5'
+      url 'https://github.com/mashiike/gdnotify/releases/download/v0.4.0/gdnotify_0.4.0_darwin_amd64.tar.gz'
+      sha256 '3bcdae739ef9fab688853f2a5349c35a1b766fb0a88a435c607ceab4df853fd5'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/gdnotify/releases/download/v0.3.0/gdnotify_0.3.0_linux_arm64.tar.gz'
-      sha256 '20290b9b9f7d564a5d7e2c169e40db82360c5004794d67b9fae795fa35cf3f92'
+      url 'https://github.com/mashiike/gdnotify/releases/download/v0.4.0/gdnotify_0.4.0_linux_arm64.tar.gz'
+      sha256 'bc2be6a5b464ccea5f2886057a5924901fa9a3448564e45ea3d6fc022878465f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/gdnotify/releases/download/v0.3.0/gdnotify_0.3.0_linux_amd64.tar.gz'
-      sha256 'b469f1b3f93f149041136b8e0342747fcf47b539928b685367d6684023e7958b'
+      url 'https://github.com/mashiike/gdnotify/releases/download/v0.4.0/gdnotify_0.4.0_linux_amd64.tar.gz'
+      sha256 '8f07fce567cd35853ab4949f351cee6d2b47b827213c2dc602be69cdeff2149c'
     end
   end
 
