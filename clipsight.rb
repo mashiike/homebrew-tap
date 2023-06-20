0d5a858a1c7b140313ff2012d106312b6fd6ab23
mashiike <ikeda-masashi@kayac.com>
2023-06-20 15:23:25 +0900

    clipsigh v0.4.0

--- clipsight.rb
+++ clipsight.rb
@@ -1,27 +1,27 @@
 class Clipsight < Formula
   desc 'Tools to share some QuickSight dashboards outside'
-  version '0.2.5'
+  version '0.4.0'
   homepage 'https://github.com/mashiike/clipsight'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/clipsight/releases/download/v0.2.5/clipsight_0.2.5_darwin_arm64.tar.gz'
-      sha256 'b72c63e7dc7b37df87c6efcc66a30b415186044e16aa14e37ef10c0047ac1111'
+      url 'https://github.com/mashiike/clipsight/releases/download/v0.4.0/clipsight_0.4.0_darwin_arm64.tar.gz'
+      sha256 'a2f12ddec5e1301654ae69ba862effb51a15a24d5d10b73352bb57e1237ad60e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/clipsight/releases/download/v0.2.5/clipsight_0.2.5_darwin_amd64.tar.gz'
-      sha256 '339fe8145c90e6a7e541a828fdf0d1ae0b63bb4d952aafff8fac1558e53b7ae7'
+      url 'https://github.com/mashiike/clipsight/releases/download/v0.4.0/clipsight_0.4.0_darwin_amd64.tar.gz'
+      sha256 '7e636a933211800b95a373cf3a6464d467f6708d6ab8122c5cb16c18fb260a6b'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/clipsight/releases/download/v0.2.5/clipsight_0.2.5_linux_arm64.tar.gz'
-      sha256 'd1df1c742c22da1762e20559ae683a5a10a5b9c753074e1e221e8a5592c8e64e'
+      url 'https://github.com/mashiike/clipsight/releases/download/v0.4.0/clipsight_0.4.0_linux_arm64.tar.gz'
+      sha256 '4bc95729d41b8166f346930564440c36db05548a2c6bc2ac080f2eb016667451'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/clipsight/releases/download/v0.2.5/clipsight_0.2.5_linux_amd64.tar.gz'
-      sha256 'b2a4871226efed776b72ab00c8eaaaca95669f6359e9092a5ddb66097fe50173'
+      url 'https://github.com/mashiike/clipsight/releases/download/v0.4.0/clipsight_0.4.0_linux_amd64.tar.gz'
+      sha256 '0fa7f55351a6dc6859d42207c374c83e77624c26b273e58a0a024fdba3cc42c4'
     end
   end
 
