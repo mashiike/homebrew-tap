351b29223cefd889a3b6b944f29fd61ab4019736
mashiike <ikeda-masashi@kayac.com>
2023-10-01 16:52:04 +0900

    update prepalert v1

--- prepalert.rb
+++ prepalert.rb
@@ -1,27 +1,27 @@
 class Prepalert < Formula
   desc 'Toil reduction tool to prepare before responding to Mackerel alerts'
-  version '0.11.0'
+  version '1.0.0'
   homepage 'https://github.com/mashiike/prepalert'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.11.0/prepalert_0.11.0_darwin_arm64.tar.gz'
-      sha256 '67f70fc8b74e26f13445f600919058eeaaad5b189d4b5a7b3affc8f0045f171b'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_darwin_arm64.tar.gz'
+      sha256 '979670e953d6ae8db62591dacbc8b623bc9c0a06102db6d0ff5ce962064275b4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.11.0/prepalert_0.11.0_darwin_amd64.tar.gz'
-      sha256 'fbc5bc07c8288d905cf002ffeb9c2c8d15b1aa1d38ed3f365953d8b49c628f7c'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_darwin_amd64.tar.gz'
+      sha256 '3f03986d862466bce845fb72caaa08be6856947bc32f0783dff50e8e1464f322'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.11.0/prepalert_0.11.0_linux_arm64.tar.gz'
-      sha256 '51f371e32d4553db97f846d8bfc390786985b80b6d409343bfbcf1a18dde65de'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_linux_arm64.tar.gz'
+      sha256 '30e22e60c4ed3791980dad0838c809c8ac271fb7cc931a4b1e360398c0c4e4ce'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v0.11.0/prepalert_0.11.0_linux_amd64.tar.gz'
-      sha256 '72b2c6a2ca393b526dc97396967c082b3a0308f1fe6ab9afd8cf45cae2658bf9'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_linux_amd64.tar.gz'
+      sha256 'b94860d109dbb0e36aad28159122be952af2f571d83f7a53b1157815bc9821c0'
     end
   end
 
