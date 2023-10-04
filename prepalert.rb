fe2f6631193b7402deb63e76beb2618bfa758fe1
mashiike <ikeda-masashi@kayac.com>
2023-10-04 14:53:23 +0900

    prepalert v1.0.1

--- prepalert.rb
+++ prepalert.rb
@@ -1,27 +1,27 @@
 class Prepalert < Formula
   desc 'Toil reduction tool to prepare before responding to Mackerel alerts'
-  version '1.0.0'
+  version '1.0.1'
   homepage 'https://github.com/mashiike/prepalert'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_darwin_arm64.tar.gz'
-      sha256 '979670e953d6ae8db62591dacbc8b623bc9c0a06102db6d0ff5ce962064275b4'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.1/prepalert_1.0.1_darwin_arm64.tar.gz'
+      sha256 '2bac63d5506d49a31413c5a34d855b0c3224b0e808e2480c3a42ec713bca38ae'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_darwin_amd64.tar.gz'
-      sha256 '3f03986d862466bce845fb72caaa08be6856947bc32f0783dff50e8e1464f322'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.1/prepalert_1.0.1_darwin_amd64.tar.gz'
+      sha256 '57e3d7558bed69070393cbd9d34440337ab89eb922259f5ab29f2f1acfe2ef59'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_linux_arm64.tar.gz'
-      sha256 '30e22e60c4ed3791980dad0838c809c8ac271fb7cc931a4b1e360398c0c4e4ce'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.1/prepalert_1.0.1_linux_arm64.tar.gz'
+      sha256 'a1fa1aaf635b746e50ca7fa43b29c9b28c7f1e4fd745e06f647c803dc6ea58a0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.0/prepalert_1.0.0_linux_amd64.tar.gz'
-      sha256 'b94860d109dbb0e36aad28159122be952af2f571d83f7a53b1157815bc9821c0'
+      url 'https://github.com/mashiike/prepalert/releases/download/v1.0.1/prepalert_1.0.1_linux_amd64.tar.gz'
+      sha256 '2b28fefdb35aefb035ca560840fb823c3f3c31abe129625a41f5dd0c267b61ec'
     end
   end
 
