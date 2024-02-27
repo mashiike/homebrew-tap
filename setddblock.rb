264b884142d444c6b5fe68769d258226ed0dd54a
mashiike <ikeda-masashi@kayac.com>
2024-02-27 16:35:13 +0900

    version up v0.5.0

--- setddblock.rb
+++ setddblock.rb
@@ -1,27 +1,27 @@
 class Setddblock < Formula
   desc 'Distributed locking using Amazon DynamoDB'
-  version '0.3.2'
+  version '0.5.0'
   homepage 'https://github.com/mashiike/setddblock'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/setddblock/releases/download/v0.3.2/setddblock_0.3.2_darwin_arm64.tar.gz'
-      sha256 '8c2bd7fe3c811ccb85dc14cdc013cf8d558bff352aa600a6e74a1de521ebb069'
+      url 'https://github.com/mashiike/setddblock/releases/download/v0.5.0/setddblock_0.5.0_darwin_arm64.tar.gz'
+      sha256 'f460fcb849321e279a8a448ed43fb3034a0da7c6feacaa6696e29d19f4423940'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/setddblock/releases/download/v0.3.2/setddblock_0.3.2_darwin_amd64.tar.gz'
-      sha256 '997565700f6e1f8e7fae622bfba983e769ea1e8b97c8e4872f52bf7b917585e4'
+      url 'https://github.com/mashiike/setddblock/releases/download/v0.5.0/setddblock_0.5.0_darwin_amd64.tar.gz'
+      sha256 '43e11305f8606af54bbeede6d6bd2348f68796b64dc7a390509fff7151682822'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/setddblock/releases/download/v0.3.2/setddblock_0.3.2_linux_arm64.tar.gz'
-      sha256 '07db4313c3ae628072aef17b4d42507d05b94016c7af6413a02dc6df0dd46607'
+      url 'https://github.com/mashiike/setddblock/releases/download/v0.5.0/setddblock_0.5.0_linux_arm64.tar.gz'
+      sha256 '2a0b89695386a9bd6a7ced924cc13c64091026528d0c5e29a91e3b7a09f04b7f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/setddblock/releases/download/v0.3.2/setddblock_0.3.2_linux_amd64.tar.gz'
-      sha256 '83e0da06af8d6a3db3f0d89520e5ec30d291d484d6c85b19168590b8beb10208'
+      url 'https://github.com/mashiike/setddblock/releases/download/v0.5.0/setddblock_0.5.0_linux_amd64.tar.gz'
+      sha256 'efca0d960a92eed58b882dd573613ae0cecbc252c24af6ab2572660b85f8c146'
     end
   end
 
