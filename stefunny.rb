98d67b1ae3befd6c12a7abbc3d20af738e787899
mashiike <ikeda-masashi@kayac.com>
2025-09-18 12:02:34 +0900

    update stefunny

--- stefunny.rb
+++ stefunny.rb
@@ -1,26 +1,26 @@
 class Stefunny < Formula
-  version '0.9.2'
+  version '0.9.3'
   homepage 'https://github.com/mashiike/stefunny'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.2/stefunny_0.9.2_darwin_arm64.tar.gz'
-      sha256 '4fb20a03bfca5ab3eb34ca77a4f1026c523ee85ef2207e4d2f7061d6f419f44e'
+      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.3/stefunny_0.9.3_darwin_arm64.tar.gz'
+      sha256 '73b169b28cc53fd9c7dbe13b4e174b9aac69b011c98a217abde44dc3ff277726'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.2/stefunny_0.9.2_darwin_amd64.tar.gz'
-      sha256 '37a6976c5f0f84eb7a6592561268c7ce27058bf8c0ec96cbfe99e46177b86c55'
+      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.3/stefunny_0.9.3_darwin_amd64.tar.gz'
+      sha256 '512f3a60a4899784a1e4fbcc03d9a3628a6d09d82c7735cb3949754c3476cb58'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.2/stefunny_0.9.2_linux_arm64.tar.gz'
-      sha256 '0eff95e494ab601e36b204e7fb59d85d9ed0989558ab13796d398144ee8d786a'
+      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.3/stefunny_0.9.3_linux_arm64.tar.gz'
+      sha256 'd1f95505cb36c6c05febce67e4a33791dea76c0dc9ebcdb7cdb1731f6da3867b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.2/stefunny_0.9.2_linux_amd64.tar.gz'
-      sha256 '6e912b4c123273176ae560edd29390e0543d18950a41d7f4a4b7924f33ec891f'
+      url 'https://github.com/mashiike/stefunny/releases/download/v0.9.3/stefunny_0.9.3_linux_amd64.tar.gz'
+      sha256 '0f0845e10c0b281708f461133b80f7e258f7484ad1aeef00f06bc711448cd322'
     end
   end
 
