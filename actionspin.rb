80ce914d8831f1c078dfc6ca0164a164e8e203bd
mashiike <ikeda-masashi@kayac.com>
2025-09-26 16:56:13 +0900

    actionspin v0.2.1

--- actionspin.rb
+++ actionspin.rb
@@ -1,27 +1,27 @@
 class Actionspin < Formula
   desc 'Bulk replace GitHub Actions references from version tags to commit hashes for locked, reproducible workflows.'
-  version '0.2.0'
+  version '0.2.1'
   homepage 'https://github.com/mashiike/actionspin'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.0/actionspin_0.2.0_darwin_arm64.tar.gz'
-      sha256 '7587966df630ad98c5f174032fbe903273ee85823fe539e10ae9afa6c3d44b4c'
+      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.1/actionspin_0.2.1_darwin_arm64.tar.gz'
+      sha256 '391411659b7ebf52cb92394701dd9ff00a28ec581a9447ab814235cd09e58d09'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.0/actionspin_0.2.0_darwin_amd64.tar.gz'
-      sha256 'cd53c5f1367d665f01dc5f20fbe25b5fb9e956192897f85b65ef90b17ac0066f'
+      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.1/actionspin_0.2.1_darwin_amd64.tar.gz'
+      sha256 'ab047742cca0cbe2378643cf3453de21ff0e9385b5864b014b8d4ee365455cda'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.0/actionspin_0.2.0_linux_arm64.tar.gz'
-      sha256 '3880f1105e7302f4c6b9d467da7a5c48a01c13f5fa1024c045bd5cdee6f9ec9e'
+      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.1/actionspin_0.2.1_linux_arm64.tar.gz'
+      sha256 '8fb80647f7c0dd79804cc24cd5b00f8b9f95bd38f1da63a837610d680d7e2477'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.0/actionspin_0.2.0_linux_amd64.tar.gz'
-      sha256 'a20f763830e23ad2186b9273b6df069ef00dbfb8d080cc22b71cd07a07439947'
+      url 'https://github.com/mashiike/actionspin/releases/download/v0.2.1/actionspin_0.2.1_linux_amd64.tar.gz'
+      sha256 '7e49a6c8274b14a6e3c87c1f8e2fb33891dcad40033466e4c0985f11d641cd50'
     end
   end
 
